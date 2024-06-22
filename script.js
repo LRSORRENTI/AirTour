019f5756b3d0d27dabd5dfeca0172ff181a2265c
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-06-22 10:50:07 -0500

    added about us section, styles need optimizing

--- script.js
+++ script.js
@@ -9,7 +9,18 @@ document.querySelector(".close-navbar-icon").addEventListener("click", () => {
 });
 
 const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
-
+// const colors = [
+//   "#6495ed",  
+//   "#7fffd4",  
+//   "#ffa07a",  
+//   "#f08080",  
+//   "#afeeee",  
+//   "#f5deb3",  
+//   "#d8bfd8",  
+//   "#e6e6fa",   
+//   "#ffb6c1",  
+//   "#ffe4e1"
+// ];
 let i = 0;
 
 Array.from(document.querySelectorAll(".nav-link")).forEach(item => {
