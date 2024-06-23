6ff8dc4c048b410bb0e363e96a9db7c4f3ba11ba
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-06-23 10:51:49 -0500

    more styling changes, also added favicon for browser tab

--- script.js
+++ script.js
@@ -4,27 +4,19 @@ document.querySelector(".open-navbar-icon").addEventListener("click", () => {
   container.classList.add("change");
 });
 
+
 document.querySelector(".close-navbar-icon").addEventListener("click", () => {
-  container.classList.remove("change");
+  document.querySelector(".container").classList.remove("change");
 });
 
 const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
-// const colors = [
-//   "#6495ed",  
-//   "#7fffd4",  
-//   "#ffa07a",  
-//   "#f08080",  
-//   "#afeeee",  
-//   "#f5deb3",  
-//   "#d8bfd8",  
-//   "#e6e6fa",   
-//   "#ffb6c1",  
-//   "#ffe4e1"
-// ];
 let i = 0;
 
 Array.from(document.querySelectorAll(".nav-link")).forEach(item => {
-  item.style.cssText = `background-color: ${colors[i++]}`;
+  item.style.backgroundColor = colors[i++];
+  item.addEventListener("click", () => {
+    document.querySelector(".container").classList.remove("change");
+  });
 });
 
 Array.from(document.querySelectorAll(".navigation-button")).forEach(item => {
